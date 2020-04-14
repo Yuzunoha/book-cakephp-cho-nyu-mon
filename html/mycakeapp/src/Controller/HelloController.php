0fcc2d72ea6eb21b8de9f9338ffe39bed6d7dc80
Yuzunoha <studysteadystandby@gmail.com>
2020-04-14 19:47:02 +0900

    クエリパラメタいけた

--- html/mycakeapp/src/Controller/HelloController.php
+++ html/mycakeapp/src/Controller/HelloController.php
@@ -8,8 +8,18 @@ class HelloController extends AppController
 {
   public $autoRender = false;
 
+  private function p($a)
+  {
+    echo $a . '<br>';
+  }
+
   public function index()
   {
-    echo 'HelloControllerのindexアクションです';
+    $id = $this->request->query['id'];
+    $pass = $this->request->query['pass'];
+
+    $this->p('HelloControllerのindexアクションです');
+    $this->p('id : ' . $id);
+    $this->p('pass : ' . $pass);
   }
 }
